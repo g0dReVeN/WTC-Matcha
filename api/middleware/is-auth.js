d8c317f5417e517d311c8de5b67cac3048b0d811
rdu-toi <rdu-toi@student.wethinkcode.co.za>
2020-07-10 10:45:27 +0200

    Busy with validation for users

--- api/middleware/is-auth.js
+++ api/middleware/is-auth.js
@@ -1,14 +1,16 @@
-const verifyToken = require('./jwtAuth').verifyToken;
+const UserModel = require('../models/user');
+
+const jwtAuth = require('./jwtAuth');
 
 module.exports = async (req, res, next) => {
   const token = req.header('Authorization') || req.body.token; // req.body.token needs to be removed after
   try {
-    const response = await verifyToken(token);
-      if (response.success === false)
-        return res.status(400).json({ success: false, msg: 'Token invalid'});
-      req.user = response.user;
-      next();
+    const response = await jwtAuth.verifyToken(token);
+    if (response.success === false)
+      return res.status(400).json({ success: false, msg: 'Token invalid' });
+    req.user = response.user;
+    next();
   } catch (e) {
-    return res.status(500).json({ success: false, msg: 'Internal server error', e});
+    return res.status(500).json({ success: false, msg: 'Internal server error', e });
   }
 }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const userController = require('../controllers/userController');
 const isAuth = require('../middleware/is-auth');
@@ -8,9 +9,58 @@ const router = express.Router();
 
 // router.get('/users', isAuth , userController.getFilteredUsers);
 
-router.patch('/edit-profile', isAuth , userController.patchEditProfile);
+router.patch('/editProfile',
+  [
+    body('username', 'Username needs to be a string between 4 and 15 characters.')
+      .optional()
+      .isString()
+      .isLength({ min: 4, max: 15 }),
+    body('firstname', 'Firstname needs to be a string between 4 and 15 characters.')
+      .optional()
+      .isString()
+      .isLength({ min: 1, max: 30 }),
+    body('lastname', 'Lastname needs to be a string between 4 and 15 characters.')
+      .optional()
+      .isString()
+      .isLength({ min: 1, max: 30 }),
+    body('email', 'Please enter a valid email.')
+      .optional()
+      .isString()
+      .normalizeEmail()
+      .isEmail(),
+    body('password', 'Password needs to be alphanumeric and between 6 and 12 characters.')
+      .optional()
+      .isString()
+      .trim()
+      .isLength({ min: 6, max: 12 })
+      .isAlphanumeric(),
+    body('age', 'Age needs to be an integer between 18 and 70.')
+      .optional()
+      .isInt({ min: 18, max: 70 }),
+    // body('location', 'Location needs to be of type JSON.')
+    //   .optional()
+    //   .isJSON(),
+    body('gender', 'Gender needs to be an integer of values \'1\' or \'2\' to represent male and female respectively.')
+      .optional()
+      .isInt({ min: 1, max: 2 }),
+    body('sexual_preference', 'Sexual preference needs to be an integer of values \'1\' or \'2\' to represent male and female respectively.')
+      .optional()
+      .isInt({ min: 1, max: 2 }),
+    body('tags', 'Tags needs to be an array of strings.')
+      .optional(),
+      // .isArray(),
+    body('biography', 'Biography needs to be a string.')
+      .optional()
+      .isString()
+  ], isAuth, userController.patchEditProfile);
 
-router.put('/addHistory', isAuth , userController.putHistory);
+router.put('/addHistory',
+[
+  body('user_id', 'User_id needs to be a string.')
+  .isInt(),
+  body('like', 'Like needs to be a boolean value')
+  .isBoolean()
+], isAuth, userController.putHistory);
 
 /** Update DB componenets and async */
 router.get('/retrieveFilteredUsers', isAuth, userController.getFilteredUsers);
